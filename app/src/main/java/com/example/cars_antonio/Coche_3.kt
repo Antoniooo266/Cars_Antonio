a39b957d716fdfc11350017dc007865de243e3e2
Antonio <acrocamora@gmail.com>
2021-11-30 10:01:26 +0100

    coches

---
+++ app/src/main/java/com/example/cars_antonio/Coche_3.kt
@@ -0,0 +1,11 @@
+package com.example.cars_antonio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Coche_3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coche3)
+    }
+}
