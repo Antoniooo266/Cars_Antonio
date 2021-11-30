8da1b7bfe5e5fec1a232aca21900dff42f9a91ae
Antonio <acrocamora@gmail.com>
2021-11-30 11:56:41 +0100

    coches

---
+++ app/src/main/java/com/example/cars_antonio/Menu_Cars.kt
@@ -0,0 +1,11 @@
+package com.example.cars_antonio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu_Cars : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_cars)
+    }
+}
